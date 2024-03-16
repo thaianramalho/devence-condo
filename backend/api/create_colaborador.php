9da8587117d92e57fbf33b1f18579841bcb3e904
Thaian Ramalho <80857637+thaianramalho@users.noreply.github.com>
2024-03-16 04:00:47 -0300

    apis create done

---
+++ backend/api/create_colaborador.php
@@ -0,0 +1,30 @@
+<?php
+// create_visitante.php
+require_once '../config/request_config.php';
+
+$senhaCorreta = "dxic5CyB";
+
+if (isset($_GET['senha']) && $_GET['senha'] === $senhaCorreta) {
+    include_once '../config/dbconfig.php';
+
+    $data = json_decode(file_get_contents('php://input'), true);
+
+    try {
+        $sql = "SELECT * FROM colaboradores WHERE cpf = ?";
+        $stmt = $connection->prepare($sql);
+        $stmt->execute([$data['cpf']]);
+        $result = $stmt->fetch();
+
+        if ($result) {
+            echo json_encode(['status' => 'error', 'message' => 'CPF já existe']);
+        } else {
+            $sql = "INSERT INTO colaboradores (nome, cpf, telefone, endereco, cargo, observacoes) VALUES (?, ?, ?, ?, ?, ?)";
+            $stmt = $connection->prepare($sql);
+            $stmt->execute([$data['nome'], $data['cpf'], $data['telefone'], $data['endereco'], $data['cargo'], $data['observacoes']]);
+
+            echo json_encode(['status' => 'success']);
+        }
+    } catch (PDOException $e) {
+        echo json_encode(['status' => 'error', 'message' => $e->getMessage()]);
+    }
+}
